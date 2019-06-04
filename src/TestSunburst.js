687b3037ce4b2e178e19bb1b5956cd9006fe61ae
Nick McCarter <Nick.McCarter@Amway.com>
2019-06-04 09:30:30 -0400

    Added code to read and log an Excel speadsheet converted to a JSON file

--- src/TestSunburst.js
+++ src/TestSunburst.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import Excel, { Workbook } from 'exceljs';
 import {Runtime, Inspector} from "@observablehq/runtime";
 import notebook from "5a3cb19b695d558f";
 
 class TestSunburst extends React.Component {
     chartRef = React.createRef();
     componentDidMount() {
+        var workbook = new Excel.Workbook();
+        workbook.xlsx.readFile("mnt/c/Users/aiuhjc9/Documents/testbook.xlsx");
+        const book = [];
+        workbook.eachSheet( sheet => {
+            const sheet = [];
+            worksheet.eachRow(row => {
+                sheet.push(row.values);
+            });
+        book.push(sheet);
+        });
+        const json = JSON.stringify(book);
+        console.log(json);
         const runtime = new Runtime();
         runtime.module(notebook, name => {
             if (name === "chart") {
